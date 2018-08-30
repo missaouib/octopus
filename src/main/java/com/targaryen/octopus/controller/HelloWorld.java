9c7c32447666d28234712df564e0436d22bdcd88
He_Junfeng <he_j@worksap.co.jp>
2018-08-30 09:32:38 +0800

    2018-08-30 1. add thymeleaf dependency
    Signed-off-by: He_Junfeng <he_j@worksap.co.jp>

--- src/main/java/com/targaryen/octopus/controller/HelloWorld.java
+++ src/main/java/com/targaryen/octopus/controller/HelloWorld.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping(value = "/octopus", produces= MediaType.TEXT_HTML_VALUE)
@@ -16,4 +17,10 @@ public class HelloWorld {
         return "Hello World!";
     }
 
+    @RequestMapping(value = "/thymeleaf")
+    public ModelAndView test(ModelAndView mv) {
+        mv.setViewName("/greeting");
+        mv.addObject("title","欢迎使用Thymeleaf!");
+        return mv;
+    }
 }
