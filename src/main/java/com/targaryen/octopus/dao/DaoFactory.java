49f915488a0ef5e7188682e49cb233edfb7e39b8
lvfai <lvfai@lvfai.com>
2018-08-30 11:42:22 +0800

    dao/: add newly added repositories to DaoFactory

--- src/main/java/com/targaryen/octopus/dao/DaoFactory.java
+++ src/main/java/com/targaryen/octopus/dao/DaoFactory.java
@@ -2,4 +2,11 @@ package com.targaryen.octopus.dao;
 
 public interface DaoFactory {
     NoteRepository getNoteRepository();
+    ApplicationDtoRepository getApplicationDtoRepository();
+    CandidateDtoRepository getCandidateDtoRepository();
+    InterviewDtoRepository getInterviewDtoRepository();
+    InterviewerDtoRepository getInterviewerDtoRepository();
+    JobDtoRepository getJobDtoRepository();
+    RoleDtoRepository getRoleDtoRepository();
+    UserDtoRepository getUserDtoRepository();
 }
--- src/main/java/com/targaryen/octopus/dao/DaoFactoryImpl.java
+++ src/main/java/com/targaryen/octopus/dao/DaoFactoryImpl.java
@@ -5,16 +5,61 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class DaoFactoryImpl implements DaoFactory{
-    private final NoteRepository noteRepository;
-
     @Autowired
-    public DaoFactoryImpl(NoteRepository noteRepository) {
-        this.noteRepository = noteRepository;
-    }
+    private NoteRepository noteRepository;
+    @Autowired
+    private ApplicationDtoRepository applicationDtoRepository;
+    @Autowired
+    private CandidateDtoRepository candidateDtoRepository;
+    @Autowired
+    private InterviewDtoRepository interviewDtoRepository;
+    @Autowired
+    private InterviewerDtoRepository interviewerDtoRepository;
+    @Autowired
+    private JobDtoRepository jobDtoRepository;
+    @Autowired
+    private RoleDtoRepository roleDtoRepository;
+    @Autowired
+    private UserDtoRepository userDtoRepository;
 
 
     @Override
     public NoteRepository getNoteRepository() {
         return noteRepository;
+    }
+
+    @Override
+    public ApplicationDtoRepository getApplicationDtoRepository() {
+        return applicationDtoRepository;
+    }
+
+    @Override
+    public CandidateDtoRepository getCandidateDtoRepository() {
+        return candidateDtoRepository;
+    }
+
+    @Override
+    public InterviewDtoRepository getInterviewDtoRepository() {
+        return interviewDtoRepository;
+    }
+
+    @Override
+    public InterviewerDtoRepository getInterviewerDtoRepository() {
+        return interviewerDtoRepository;
+    }
+
+    @Override
+    public JobDtoRepository getJobDtoRepository() {
+        return jobDtoRepository;
+    }
+
+    @Override
+    public RoleDtoRepository getRoleDtoRepository() {
+        return roleDtoRepository;
+    }
+
+    @Override
+    public UserDtoRepository getUserDtoRepository() {
+        return userDtoRepository;
     }
 }
