b062b6051ff0354647ae2f4fce82bd7e248b6adf
liu_me <liu_me@worksap.co.jp>
2018-09-04 10:45:53 +0800

    refs#26: Create PublicServiceImpl

--- src/main/java/com/targaryen/octopus/service/PublicServiceImpl.java
+++ src/main/java/com/targaryen/octopus/service/PublicServiceImpl.java
@@ -1,4 +1,19 @@
 package com.targaryen.octopus.service;
 
+import com.targaryen.octopus.dao.DaoFactory;
+import com.targaryen.octopus.dao.PostDtoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ *  Created by Liu Mengyang on 2018/09/04
+ */
+@Service
 public class PublicServiceImpl {
+    private PostDtoRepository postDtoRepository;
+
+    @Autowired
+    public PublicServiceImpl(DaoFactory daoFactory) {
+        this.postDtoRepository = daoFactory.getPostDtoRepository();
+    }
 }
