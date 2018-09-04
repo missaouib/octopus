bf4b3695f2c6662ca4aad0bbfaeb1e8eacd15904
liu_me <liu_me@worksap.co.jp>
2018-09-04 16:31:02 +0800

    '#26: Update util.status'

---
+++ src/main/java/com/targaryen/octopus/util/status/ApplicationStatus.java
@@ -0,0 +1,8 @@
+package com.targaryen.octopus.util.status;
+
+public class ApplicationStatus {
+    final static public Integer INIT = 0;
+    final static public Integer FILTED = 1;
+    final static public Integer PASSED = 2;
+    final static public Integer FAILED = -1;
+}
