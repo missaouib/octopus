4d1c8cf3db5e5a66cf6bb0a27d0c21e2a75aaa80
liu_me <liu_me@worksap.co.jp>
2018-09-04 11:36:58 +0800

    refs:#26 Update PublicServiceImpl
    1. Set PublicServiceImpl implements PublicService.
    2. Implement listPostsByStatus.
    
    Signed-off-by: liu_me <liu_me@worksap.co.jp>

--- src/main/java/com/targaryen/octopus/service/PublicServiceImpl.java
+++ src/main/java/com/targaryen/octopus/service/PublicServiceImpl.java
@@ -2,18 +2,38 @@ package com.targaryen.octopus.service;
 
 import com.targaryen.octopus.dao.DaoFactory;
 import com.targaryen.octopus.dao.PostDtoRepository;
+import com.targaryen.octopus.dto.PostDto;
+import com.targaryen.octopus.util.status.PostStatus;
+import com.targaryen.octopus.vo.PostVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  *  Created by Liu Mengyang on 2018/09/04
  */
 @Service
-public class PublicServiceImpl {
+public class PublicServiceImpl implements PublicService {
     private PostDtoRepository postDtoRepository;
 
     @Autowired
     public PublicServiceImpl(DaoFactory daoFactory) {
         this.postDtoRepository = daoFactory.getPostDtoRepository();
+    }
+
+    @Override
+    public List<PostVo> listPostsByStatus(int status) {
+        List<PostDto> postList = postDtoRepository.findPostDtoByStatus(PostStatus.POSTED);
+
+        return postList.stream().map( x -> new PostVo.Builder()
+                .postId(x.getPostId())
+                .postLocale(x.getPostLocale())
+                .postName(x.getPostName())
+                .postRequirement(x.getPostRequirement())
+                .postType(x.getPostType())
+                .postDescription(x.getPostDescription())
+                .status(x.getStatus()).build()).collect(Collectors.toList());
     }
 }
