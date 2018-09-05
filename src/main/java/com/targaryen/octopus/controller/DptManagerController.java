d261a793af3c61b8dc9cffc18c00c68f7928dba7
zhao_we <zhao_we@worksap.co.jp>
2018-09-05 11:33:38 +0800

    #22 Updated dpt-index and dpt-post-list

--- src/main/java/com/targaryen/octopus/controller/DptManagerController.java
+++ src/main/java/com/targaryen/octopus/controller/DptManagerController.java
@@ -1,22 +1,19 @@
 package com.targaryen.octopus.controller;
 
-import com.targaryen.octopus.dto.DptManagerDto;
 import com.targaryen.octopus.dto.PostDto;
 import com.targaryen.octopus.entity.PostEntity;
 import com.targaryen.octopus.security.AuthInfo;
 import com.targaryen.octopus.service.DptManagerService;
 import com.targaryen.octopus.service.ServiceFactory;
+import com.targaryen.octopus.util.Role;
+import com.targaryen.octopus.util.StatusCode;
 import com.targaryen.octopus.vo.PostVo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping(value = "/octopus", produces= MediaType.TEXT_HTML_VALUE)
@@ -28,8 +25,16 @@ public class DptManagerController {
         this.dptManagerService = serviceFactory.getDptManagerService();
     }
 
+    @RequestMapping(value="/dpt/index")
+    public ModelAndView dptIndex(ModelMap map){
+        ModelAndView result = new ModelAndView("dpt-index");
+        map.addAttribute("roleName", Role.getRoleNameByAuthority());
+        map.addAttribute("userName", AuthInfo.getUserName());
+        return result;
+    }
+
     @RequestMapping(value = "/dpt/post/add", method = RequestMethod.GET)
-    public String postAddGet(ModelMap map) {
+    public String dptPostAddGet(ModelMap map) {
         /* UI Settings */
         map.addAttribute("title", "Add new post need");
         map.addAttribute("action", "add");
@@ -43,7 +48,7 @@ public class DptManagerController {
 
     @RequestMapping(value = "/dpt/post/add", method = RequestMethod.POST)
     @ResponseBody
-    public String postAddPost(PostEntity postEntity) {
+    public String dptPostAddPost(PostEntity postEntity) {
         PostVo postVo = new PostVo.Builder()
                 .postId(postEntity.getPostId())
                 .postName(postEntity.getPostName())
@@ -62,13 +67,13 @@ public class DptManagerController {
     }
 
     @RequestMapping("/dpt/post/list")
-    public String postList(ModelMap map) {
+    public String dptPostList(ModelMap map) {
         map.addAttribute("postList", dptManagerService.findPostsByDptManagerId(AuthInfo.getUserId()));
         return "dpt-post-list";
     }
 
     @RequestMapping(value = "/dpt/post/detail/{postId}", method = RequestMethod.GET)
-    public String postDetail(@PathVariable("postId") int postId, ModelMap map) {
+    public String dptPostDetail(@PathVariable("postId") int postId, ModelMap map) {
         /* UI Settings */
         map.addAttribute("title", "Check/Edit post need detail");
         map.addAttribute("action", "../edit");
@@ -82,7 +87,7 @@ public class DptManagerController {
 
     @RequestMapping(value = "/dpt/post/edit", method = RequestMethod.POST)
     @ResponseBody
-    public String postEditPost(PostEntity postEntity) {
+    public String dptPostEditPost(PostEntity postEntity) {
         PostVo postVo = new PostVo.Builder()
                 .postId(postEntity.getPostId())
                 .postName(postEntity.getPostName())
@@ -97,6 +102,10 @@ public class DptManagerController {
                 .build();
         
         int result = dptManagerService.updatePost(postVo);
-        return "OK";
+        if (result == StatusCode.SUCCESS) {
+            return "OK";
+        } else {
+            return "Fail";
+        }
     }
 }
