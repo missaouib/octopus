51eed0e023b1f3575106b659d9930bc65182f752
He_Junfeng <he_j@worksap.co.jp>
2018-09-05 11:14:05 +0800

    ref:#23 add resume filter service methods

--- src/main/java/com/targaryen/octopus/dao/InterviewerDtoRepository.java
+++ src/main/java/com/targaryen/octopus/dao/InterviewerDtoRepository.java
@@ -9,4 +9,5 @@ import org.springframework.stereotype.Repository;
  * */
 @Repository
 public interface InterviewerDtoRepository extends JpaRepository<InterviewerDto, Integer> {
+    public InterviewerDto findInterviewerDtoByInterviewerId(Integer id);
 }
