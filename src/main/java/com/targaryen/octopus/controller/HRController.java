8d4ef4d5b4fcfe8f8eb85ead09ae2ae7df7ec5bc
zhao_we <zhao_we@worksap.co.jp>
2018-09-05 16:16:44 +0800

    refs:#22 Finish publish/close/edit

--- src/main/java/com/targaryen/octopus/controller/HRController.java
+++ src/main/java/com/targaryen/octopus/controller/HRController.java
@@ -1,9 +1,12 @@
 package com.targaryen.octopus.controller;
 
+import com.targaryen.octopus.entity.PostEntity;
 import com.targaryen.octopus.security.AuthInfo;
 import com.targaryen.octopus.service.HRService;
 import com.targaryen.octopus.service.ServiceFactory;
 import com.targaryen.octopus.util.Role;
+import com.targaryen.octopus.util.StatusCode;
+import com.targaryen.octopus.vo.PostVo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
@@ -11,6 +14,7 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -38,21 +42,46 @@ public class HRController {
     }
 
     @RequestMapping(value = "/hr/post/detail/{postId}", method = RequestMethod.GET)
-    public String dptPostDetail(@PathVariable("postId") int postId, ModelMap map) {
+    public String hrPostDetail(@PathVariable("postId") int postId, ModelMap map) {
         /* UI Settings */
-        map.addAttribute("title", "HR - Check/Edit post need detail");
+        map.addAttribute("title", "Check/Edit post need detail");
         map.addAttribute("action", "../edit");
         map.addAttribute("returnUrl", "../list");
         map.addAttribute("swalTextSuccess", "You have successfully edited this post need!");
         map.addAttribute("swalTextFailure", "You have not successfully edited this post need.");
 
         map.addAttribute("post", hrService.findPostById(postId));
-        return "hr-post-detail";
+        return "dpt-hr-post-detail";
     }
 
-    @RequestMapping(value = "/hr/post/publish/{postId}", method = RequestMethod.GET)
+    @RequestMapping(value = "/hr/post/publish/{postId}", method = RequestMethod.POST)
     public String hrPostPublish(@PathVariable("postId") int postId) {
-        hrService.publishPostById(postId);
+        int status = hrService.publishPostById(postId);
         return "redirect:../list";
+    }
+
+    @RequestMapping(value = "/hr/post/close/{postId}", method = RequestMethod.POST)
+    public String hrPostClose(@PathVariable("postId") int postId) {
+        int status = hrService.closePostById(postId);
+        return "redirect:../list";
+    }
+
+    @RequestMapping(value = "/hr/post/edit", method = RequestMethod.POST)
+    @ResponseBody
+    public String hrPostEditPost(PostEntity postEntity) {
+        PostVo postVo = new PostVo.Builder()
+                .postId(postEntity.getPostId())
+                .postName(postEntity.getPostName())
+                .postType(postEntity.getPostType())
+                .postLocale(postEntity.getPostLocale())
+                .postDescription(postEntity.getPostDescription())
+                .postRequirement(postEntity.getPostRequirement())
+                .recruitNum(postEntity.getRecruitNum())
+                .recruitDpt(postEntity.getRecruitDpt())
+                .publishTime(postEntity.getPublishTime())
+                .status(postEntity.getStatus())
+                .build();
+
+        return String.valueOf(hrService.updatePost(postVo));
     }
 }
