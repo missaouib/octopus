2689da2da7c0031f7b39277548e2e5f19c003dea
He_Junfeng <he_j@worksap.co.jp>
2018-09-05 15:44:56 +0800

    ref:#32 Changed ApplicationStatus

--- src/main/java/com/targaryen/octopus/util/status/ApplicationStatus.java
+++ src/main/java/com/targaryen/octopus/util/status/ApplicationStatus.java
@@ -2,7 +2,12 @@ package com.targaryen.octopus.util.status;
 
 public class ApplicationStatus {
     final static public Integer INIT = 0;
-    final static public Integer FILTED = 1;
-    final static public Integer PASSED = 2;
-    final static public Integer FAILED = -1;
+    final static public Integer FILTER_PASS = 1;
+    final static public Integer FILTER_FAIL = -1;
+    final static public Integer INTERVIEW_PASS = 2;
+    final static public Integer INTERVIEW_FAIL = -2;
+    final static public Integer DPT_PASS = 3;
+    final static public Integer DPT_FAIL = -3;
+    final static public Integer APPLICANT_ACCEPT = 4;
+    final static public Integer APPLICANT_REJECT = -4;
 }
