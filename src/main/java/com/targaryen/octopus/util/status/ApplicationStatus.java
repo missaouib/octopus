a21af03289e6cb61dd126c45cb272ca6a433e6b9
liu_me <liu_me@worksap.co.jp>
2018-09-06 15:07:02 +0800

    Add toString method to octopus.util.status.*
    Signed-off-by: liu_me <liu_me@worksap.co.jp>

--- src/main/java/com/targaryen/octopus/util/status/ApplicationStatus.java
+++ src/main/java/com/targaryen/octopus/util/status/ApplicationStatus.java
@@ -8,6 +8,32 @@ public class ApplicationStatus {
     final static public Integer INTERVIEW_FAIL = -2;
     final static public Integer DPT_PASS = 3;
     final static public Integer DPT_FAIL = -3;
-    final static public Integer APPLICANT_ACCEPT = 4;
-    final static public Integer APPLICANT_REJECT = -4;
+    final static public Integer OFFER = 4;
+    final static public Integer APPLICANT_ACCEPT = 5;
+    final static public Integer APPLICANT_REJECT = -5;
+    
+    public static String toString(int status) {
+        if(status == INIT)
+            return "init";
+        else if(status == FILTER_PASS)
+            return "filter pass";
+        else if(status == FILTER_FAIL)
+            return "filter fail";
+        else if(status == INTERVIEW_PASS)
+            return "interview pass";
+        else if(status == INTERVIEW_FAIL)
+            return "interview fail";
+        else if(status == DPT_PASS)
+            return "department manager pass";
+        else if(status == DPT_FAIL)
+            return "department manager fail";
+        else if(status == OFFER)
+            return "offer sent";
+        else if(status == APPLICANT_ACCEPT)
+            return "applicant accept";
+        else if(status == APPLICANT_REJECT)
+            return "applicant reject";
+        else
+            return "invalid";
+    }
 }
--- src/main/java/com/targaryen/octopus/util/status/InterviewStatus.java
+++ src/main/java/com/targaryen/octopus/util/status/InterviewStatus.java
@@ -4,4 +4,15 @@ public class InterviewStatus {
     final static public Integer INIT = 0;
     final static public Integer FAIL = -1;
     final static public Integer SUCCESS = 1;
+
+    public static String toString(int status) {
+        if (status == INIT)
+            return "pending";
+        else if (status == FAIL)
+            return "fail";
+        else if (status == SUCCESS)
+            return "success";
+        else
+            return "invalid";
+    }
 }
