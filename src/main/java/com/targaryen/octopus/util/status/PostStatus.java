062d0ec7d3b23532c84aaa593eccbe2de0a9d9b7
He_Junfeng <he_j@worksap.co.jp>
2018-09-06 12:54:11 +0800

    refs:#23 Added post finished status and cheanged ApplicationResumeVo

--- src/main/java/com/targaryen/octopus/util/status/PostStatus.java
+++ src/main/java/com/targaryen/octopus/util/status/PostStatus.java
@@ -1,6 +1,7 @@
 package com.targaryen.octopus.util.status;
 
 public class PostStatus {
+    final static public Integer FINISHED = 2;
     final static public Integer PUBLISHED = 1;
     final static public Integer REVOKED = -1;
     final static public Integer INIT = 0;
