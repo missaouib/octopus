9280286283e502c7a0440eaa7289b1949e01f391
liu_me <liu_me@worksap.co.jp>
2018-09-10 16:13:14 +0800

    Update InterviewResultStatus
    Signed-off-by: liu_me <liu_me@worksap.co.jp>

--- src/main/java/com/targaryen/octopus/util/status/InterviewResultStatus.java
+++ src/main/java/com/targaryen/octopus/util/status/InterviewResultStatus.java
@@ -2,16 +2,22 @@ package com.targaryen.octopus.util.status;
 
 public class InterviewResultStatus {
     final static public Integer INIT = 0;
-    final static public Integer FAIL = -1;
-    final static public Integer PASS = 1;
+    final static public Integer NOT_HIRE = -1;
+    final static public Integer WEAK_HIRE = 1;
+    final static public Integer HIRE = 2;
+    final static public Integer STRONG_HIRE = 3;
 
     public static String toString(int status) {
         if(status == INIT)
             return "not evaluated";
-        else if (status == FAIL)
-            return "fail";
-        else if (status == PASS)
-            return "pass";
+        else if (status == NOT_HIRE)
+            return "not hire";
+        else if (status == WEAK_HIRE)
+            return "weak hire";
+        else if (status == HIRE)
+            return "hire";
+        else if (status == STRONG_HIRE)
+            return "strong hire";
         else
             return "invalid";
     }
