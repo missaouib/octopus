9894437a60e6388ef404a1cd77882e523743e796
liu_me <liu_me@worksap.co.jp>
2018-09-11 12:44:28 +0800

    Add anotation for repositories
    Signed-off-by: liu_me <liu_me@worksap.co.jp>

--- src/main/java/com/targaryen/octopus/dao/DptManagerDtoRepository.java
+++ src/main/java/com/targaryen/octopus/dao/DptManagerDtoRepository.java
@@ -2,10 +2,12 @@ package com.targaryen.octopus.dao;
 
 import com.targaryen.octopus.dto.DptManagerDto;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 /**
  *  @author He Junfeng
  * */
+@Repository
 public interface DptManagerDtoRepository extends JpaRepository<DptManagerDto, Integer> {
     DptManagerDto findDptManagerDtoByDptManagerId(Integer id);
 }
