246b97618f88b20cb64237ef8028e61b72997cab
He_Junfeng <he_j@worksap.co.jp>
2018-09-12 17:45:44 +0800

    refs:#79 Added data in data.sql

--- src/main/java/com/targaryen/octopus/applicationRunner/MyApplicationRunner.java
+++ src/main/java/com/targaryen/octopus/applicationRunner/MyApplicationRunner.java
@@ -13,33 +13,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class MyApplicationRunner implements ApplicationRunner {
-    private DptManagerService dptManagerService;
-    private ApplicantService applicantService;
-
-
-    @Autowired
-    public MyApplicationRunner(ServiceFactory serviceFactory) {
-        this.dptManagerService = serviceFactory.getDptManagerService();
-        this.applicantService = serviceFactory.getApplicantService();
-    }
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        /*
-        DptManagerDto dptManager = new DptManagerDto();
-        dptManager.setDptManagerId(1);
-        dptManagerService.saveDptManager(dptManager);
-
-        PostDto post = new PostDto();
-        post.setPostId(1);
-        post.setPostName("Java Developer");
-        post.setPostType("Engineer");
-        post.setPostLocale("Tokyo");
-        post.setPostDescription("Are you passionate about technology in its many forms? Are you excited by interacting with people throughout the day, in person, on the phone, via email, and chat?");
-        post.setPostRequirement("Unfortunately you are not.");
-        post.setStatus(1);
-        post.setDptManager(dptManager);
-        dptManagerService.createNewPost(post);
-        */
     }
 }
