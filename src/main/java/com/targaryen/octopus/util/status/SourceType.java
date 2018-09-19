3b5d82078c3d7b2fbc89e9077529331e70721a2a
liu_me <liu_me@worksap.co.jp>
2018-09-19 12:16:09 +0800

    Add new source type to SourceType
    Signed-off-by: liu_me <liu_me@worksap.co.jp>

--- src/main/java/com/targaryen/octopus/util/status/SourceType.java
+++ src/main/java/com/targaryen/octopus/util/status/SourceType.java
@@ -3,12 +3,15 @@ package com.targaryen.octopus.util.status;
 public class SourceType {
     public final static Integer SCHOOL = 0;
     public final static Integer CITY = 1;
+    public final static Integer POST = 2;
 
     public static String toString(int status) {
         if(status == SCHOOL)
             return "school";
         else if(status == CITY)
             return "city";
+        else if(status == POST)
+            return "post";
         else
             return "invalid";
     }
